eb043bf29fe65f094dafdce908789704a8cf9a6a
Oskar <oskar.e.moberg@proton.me>
2026-03-16 23:52:22 +0100

    Initial BO sketch

--- bayesian_optim.py
+++ bayesian_optim.py
@@ -1,118 +1,104 @@
 from bayes_opt import BayesianOptimization
-import matplotlib.pyplot as plt
-import numpy as np
+from dataclasses import dataclass, asdict
+import tyro
+import train
 
-# from train import main
+DEFAULT_STATE_PATH = "./bayes_optimizer_state.json"
+BINARY_MILLION = 2**20
 
 
-def black_box_function(x, y, k):
-    """Function with unknown internals we wish to maximize.
+@dataclass
+class CliArgs:
+  # General
+  seed: int = 1000
+  logging_level: int = 2
+  random_init_probes: int = 0
+  bo_iters: int = 3
 
-    This is just serving as an example, for all intents and
-    purposes think of the internals of this function, i.e.: the process
-    which generates its output values, as unknown.
-    """
-    return -x ** 2 - (y - 1) ** 2 + (int(k) * 4)
+  # Saving and loading
+  should_save_state: bool = False
+  should_load_state: bool = False
+  save_path: str = DEFAULT_STATE_PATH
+  load_path: str = DEFAULT_STATE_PATH
 
+  # Non-optimised hyperparams
+  num_epochs: int = 64
+  num_envs: int = 128
+  num_eval_envs: int = 128
+  total_env_steps: int = 8 * BINARY_MILLION
+
+
+def main():
+  # Read args from commandline
+  cli_args = tyro.cli(CliArgs)
+
+  # Wrap train.main with a function that only takes the params to be optimised
+  def train_optim_wrapper(
+    batch_size: int,
+    unroll_length: int,
+    actor_lr: float,
+    critic_lr: float,
+    alpha_lr: float,
+  ) -> float:
+
+    return train.main(
+      train.Args(
+        # Not to be optimised
+        num_epochs=cli_args.num_epochs,
+        num_envs=cli_args.num_envs,
+        num_eval_envs=cli_args.num_eval_envs,
+        total_env_steps=cli_args.total_env_steps,
+        # To be optimised
+        batch_size=batch_size,
+        unroll_length=unroll_length,
+        actor_lr=actor_lr,
+        critic_lr=critic_lr,
+        alpha_lr=alpha_lr,
+      )
+    )
+
+  param_bounds = {
+    "x": (2, 4),
+    "y": (-3, 3, int),
+    "k": ("1", "2"),
+  }
+
+  optimizer = BayesianOptimization(
+    f=train_optim_wrapper,
+    pbounds=param_bounds,
+    random_state=cli_args.seed,
+    verbose=cli_args.logging_level
+  )
+
+  if cli_args.should_load_state:
+    optimizer.load_state(cli_args.load_path)
+
+  # Points to manually probe
+  probes = [
+    {"x": 0.5, "y": 2, "k": "1"},
+    {"x": 0.5, "y": 2, "k": "1"},
+  ]
+
+  # Queues points to be probed upon maximize()
+  for probe in probes:
+    optimizer.probe(
+      params=probe,
+      lazy=True,
+    )
+
+  # Optim
+  optimizer.maximize(
+    init_points=cli_args.random_init_probes,
+    n_iter=cli_args.bo_iters,
+  )
+
+  # The best combination of parameters and target value found
+  print("Best target:", optimizer.max["target"])
+  print("with params:", {key: round(value.item(), ndigits=3) for key, value in optimizer.max["params"].items()})
+
+  # Save state
+  if cli_args.should_save_state:
+    optimizer.save_state(cli_args.save_path)
 
 if __name__ == "__main__":
-    # Parameter bounds,
-    pbounds = {
-        "x": (2, 4),
-        "y": (-3, 3, int), # Supply type for contrained optim
-        "k": ("1", "2"), # Categorical params via strings
-    }
-
-    # Create the optimiser
-    optimizer = BayesianOptimization(
-        f=black_box_function,
-        pbounds=pbounds,
-        verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
-        random_state=1,
-    )
-
-    # Do optim
-    optimizer.maximize(
-        init_points=2, # Num random points to probe before optim
-        n_iter=3, # Num bayesian optim steps
-    )
-
-    # The best combination of parameters and target value found
-    initial_best = optimizer.max
-    print("Initial best target:", initial_best["target"])
-    print("with params:", {key: round(value.item(),ndigits=3) for key, value in initial_best["params"].items()})
-
-    # List of all parameters probed and their corresponding target values
-    # for i, res in enumerate(optimizer.res):
-    #     print(f"Iteration {i}: \n\t{res}")
-
-    # Setting new bounds
-    # optimizer.set_bounds(new_bounds={"x": (-2, 3)})
-
-    # Manually probe point
-    # optimizer.probe(
-    #     params={"x": 0.5, "y": 2, "k": "1"},
-    #     lazy=True,
-    # )
-
-    # Re-run optim without random probing
-    optimizer.maximize(
-        init_points=0,
-        n_iter=5,
-    )
-
-    # The best combination of parameters and target value found
-    new_best = optimizer.max
-    print("New best target:", new_best["target"])
-    print("with params:", {key: round(value.item(), ndigits=3) for key, value in new_best["params"].items()})
-
-    # Get the probed points for scatter
-    res = optimizer._space.res()
-    k1 = np.array([[p['params']['x'], p['params']['y']] for p in res if p['params']['k']=='1'])
-    k2 = np.array([[p['params']['x'], p['params']['y']] for p in res if p['params']['k']=='2'])
-
-    # Plot the actual function
-    x1 = np.linspace(pbounds['x'][0], pbounds['x'][1], 1000)
-    x2 = np.linspace(pbounds['y'][0], pbounds['y'][1], 1000)
-
-    X1, X2 = np.meshgrid(x1, x2)
-    Z1 = black_box_function(X1, X2, '1')
-    Z2 = black_box_function(X1, X2, '2')
-
-    fig, axs = plt.subplots(1, 2)
-
-    vmin = np.min([np.min(Z1), np.min(Z2)])
-    vmax = np.max([np.max(Z1), np.max(Z2)])
-
-    axs[0].contourf(X1, X2, Z1, vmin=vmin, vmax=vmax)
-    axs[0].set_aspect("equal")
-    axs[0].scatter(k1[:,0], k1[:,1], c='k')
-    axs[1].contourf(X1, X2, Z2, vmin=vmin, vmax=vmax)
-    axs[1].scatter(k2[:,0], k2[:,1], c='k')
-    axs[1].set_aspect("equal")
-    axs[0].set_title('k=1')
-    axs[1].set_title('k=2')
-    fig.tight_layout()
-
-    # Show the function(s)
-    plt.show()
-
-    # # Save state
-    # optimizer.save_state("optimizer_state.json")
-    #
-    # # Re-load state
-    # new_optimizer = BayesianOptimization(
-    #     f=black_box_function,
-    #     pbounds={"x": (-2, 3), "y": (-3, 3)},
-    #     random_state=1,
-    #     verbose=0
-    # )
-    # new_optimizer.load_state("./optimizer_state.json")
-    #
-    # # Continue optimization
-    # new_optimizer.maximize(
-    #     init_points=0,
-    #     n_iter=5
-    # )
-
-
+  main()
