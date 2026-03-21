3f112978a600f80d4a0e5ccbec43d8067f096dd9
Oskar <oskar.e.moberg@proton.me>
2026-03-21 16:07:48 +0100

    Bayes prints and renaming

--- bayesian_optim.py
+++ bayesian_optim.py
@@ -13,11 +13,11 @@ class CliArgs:
   seed: int = 1000
   logging_level: int = 2
   random_init_probes: int = 0
-  bo_iters: int = 16
+  bo_iters: int = 8
 
   # Saving and loading
-  should_save_state: bool = True
-  should_load_state: bool = True
+  should_save_state: bool = False
+  should_load_state: bool = False
   save_path: str = DEFAULT_STATE_PATH
   load_path: str = DEFAULT_STATE_PATH
 
@@ -25,10 +25,10 @@ class CliArgs:
   num_epochs: int = 64
   num_envs: int = 128
   num_eval_envs: int = 128
-  total_env_steps: int = 16 * BINARY_MILLION
+  iter_env_steps: int = 8 * BINARY_MILLION
 
   # Target metric to optimise for
-  target_metric: str = "eval/episode_reward"
+  target_metric: str = "eval/episode_success"
   target_denominator: str = "training/walltime"
 
 
@@ -47,10 +47,12 @@ def main():
 
   # Points to manually probe
   probes = [
-    {"batch_size": 128, "unroll_length": 64, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
-    {"batch_size": 256, "unroll_length": 128, "actor_lr": 3e-5, "critic_lr": 3e-5, "alpha_lr": 3e-5}
-    # Loosly based on best in previous optim round
-    {"batch_size": 2048, "unroll_length": 256, "actor_lr": 3e-3, "critic_lr": 3e-5, "alpha_lr": 3e-5}
+    # {"batch_size": 128, "unroll_length": 64, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
+    # {"batch_size": 256, "unroll_length": 128, "actor_lr": 3e-5, "critic_lr": 3e-5, "alpha_lr": 3e-5},
+    {"batch_size": 2048, "unroll_length": 256, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
+    {"batch_size": 16, "unroll_length": 256, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
+    {"batch_size": 2048, "unroll_length": 8, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
+    {"batch_size": 16, "unroll_length": 8, "actor_lr": 3e-4, "critic_lr": 3e-4, "alpha_lr": 3e-4},
   ]
 
   # Wrap train.main with a function that only takes the params to be optimised
@@ -71,7 +73,7 @@ def main():
         num_epochs=cli_args.num_epochs,
         num_envs=cli_args.num_envs,
         num_eval_envs=cli_args.num_eval_envs,
-        total_env_steps=cli_args.total_env_steps,
+        total_env_steps=cli_args.iter_env_steps,
         return_metric=cli_args.target_metric,
         return_denominator=cli_args.target_denominator,
         # To be optimised
@@ -92,6 +94,7 @@ def main():
   )
 
   if cli_args.should_load_state:
+    print("Loading optimiser state saved at:", cli_args.load_path)
     optimizer.load_state(cli_args.load_path)
 
   # Queues points to be probed upon maximize()
@@ -113,6 +116,7 @@ def main():
 
   # Save state
   if cli_args.should_save_state:
+    print("Saving optimiser state to:", cli_args.load_path)
     optimizer.save_state(cli_args.save_path)
 
 if __name__ == "__main__":
